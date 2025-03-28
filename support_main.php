49c0b5550c5f534977bb7b6480e00f019f284c8b
Venky <mail2venky542@gmail.com>
2025-03-28 19:42:15 +0530

    support page updated

---
+++ support_main.php
@@ -0,0 +1,74 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+            <div class="clear"></div>
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">We Support Cell </h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Support</div>
+                        <!-- /Page navigation-->
+                    </div>
+
+                </div>
+                <!-- /sub page header-->
+
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Complete Customer Care</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/about.png" width="220" height="137"
+                                    class="alignleft" /></a>
+                            We enjoy hearing from our customers and visitors. Visitors please check our Frequently Asked
+                            Questions for general information regarding our services. If your questions are still
+                            unanswered, you may contact the department of your choice listed below. </p>
+                        <p align="justify">Email is checked and online orders are processed 24 hours a day, 7 days a
+                            week and 365 days a year.For hosting related issues, the customers are requested to visit
+                            support area @ aalphanet.com It includes helpful information for your Linux and Windows 2003
+                            web hosting accounts.</p>
+
+                        <h3> Hansuja Solutionssolutions.com - Direct E-Mail Contacts</h3>
+                        <h4>Email us 24 hours a day, 7 days a week, 365 days a year.</h4>
+
+                        <p align="justify">
+                            Successful project begins from its idea development and implementation planning. Before we
+                            offer a solution, Hansuja Solutions analyze the core of your project, its goals and
+                            perspectives, individual specific needs. Whatever you may need – custom software
+                            development, business management solution integration, mobile or web application
+                            development, we are here to provide you with necessary solution. Here you can get any
+                            software development life cycle services – from project idea elaboration and planning to
+                            implementation and ongoing support. </p>
+
+                        <br />
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
