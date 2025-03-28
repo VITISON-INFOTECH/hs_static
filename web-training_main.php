5edb49f3a48fcd26353399c66fb1213fa8d72a57
Venky <mail2venky542@gmail.com>
2025-03-28 20:54:33 +0530

    web training page updated

---
+++ web-training_main.php
@@ -0,0 +1,90 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Web Training We Offer</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Web Training
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Web Technologies - Java Development Services</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/java.jpg" width="220" height="137"
+                                    class="alignleft" /></a>
+                            Java development services from ShreshtaInfotech include full cycle of software development
+                            from consulting to product support. Our Java Programmers can make high-end applications that
+                            conform to your plans and business goals.</p>
+                        <p align="justify">Using Java we can develop custom application for systems and websites used
+                            within your company, including networking and collaboration tools like chats, online
+                            conferences applications. Should you need to develop a brand new mobile Java application we
+                            are here to implement your idea and help make it as creative as possible.
+                            <br>
+                            We produce Java Solutions for a wide range of industry verticals. Due to High Level of
+                            Security and data protection Java solutions are remarkable tools for enterprises since it
+                            gives protection to all confidential information and data both for offline applications and
+                            during online exchange. Java Software runs almost on every OS, existing today, both for PC
+                            and mobile devices.
+                        </p>
+
+                        <h2>Web Technologies - PHP Development Services</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/php.jpg" width="220" height="137"
+                                    class="alignleft" /></a>
+                            At Hansuja Solutions we have talented developers to work on your project PHP Development to
+                            produce high quality PHP Solutions. Hansuja Solutions is also providing PHP Development
+                            Services from its inception in 2014 and today we are a seasoned PHP Development company.</p>
+                        <p align="justify">Our PHP Experts with up to 10+ years of experience ready to serve your
+                            project from all aspects: programmers, project managers, business analysts, QA Engineers,
+                            support engineers. Development of multifunctional software applicable for many areas, for
+                            example, e-learning, real estate, media streaming, social communities/networks. Check out
+                            our PHP portfolio.
+                        </p>
+                        <h2>Web Technologies - AJS Development Services</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/angularjs.png" width="220" height="137"
+                                    class="alignleft" /></a>
+                            AngularJS is a JavaScript Framework originally created by Google which actively support its
+                            development. The main idea of AngularJS is to combine well-known components, patterns and
+                            development practices in one framework that is straightforward to use and encourages
+                            following the best practices of web development.”</p>
+                        <p align="justify"> The underlying goal is that with AngularJS Developers you can and will
+                            choose not just any solution that works, but the best known solution, the “Angular way of
+                            doing things.</p>
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
