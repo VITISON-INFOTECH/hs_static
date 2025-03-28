1a2c29045e741d81ea32a2265a7f46519edc99ad
Venky <mail2venky542@gmail.com>
2025-03-28 21:18:25 +0530

    matrimonial page updated

---
+++ matrimonial_main.php
@@ -0,0 +1,78 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Matrimony Website Pack in PHP with MYSql</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Matrimonial
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Matrimonial Management Package System : Matri4Web 9.04 (now with SMS)</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/wedd.jpg" width="220" height="137"
+                                    class="alignleft" /></a>
+                            This is the best solution if you are looking to run a matrimonial services website. We have
+                            integrated many features which you can use and take full advantage of the matrimonial
+                            services. </p>
+                        <p align="justify">This is a very powerful online matrimony management package system that we've
+                            developed exclusively and user friendly. User will be able to search, update, add/remove,
+                            and edit their profiles from database. In addition, user can also add/delete/change
+                            descriptions, upload images/photos.</p>
+
+                        <h3>What We Offer</h3>
+                        <ul>
+                            <li>Member registration (paid or free)</li>
+                            <li>Browse categories that are sorted by region, community,
+                                religion Basic search on homepage</li>
+                            <li>Advanced search with additional parameters</li>
+                            <li>Featured profile listings on homepage along with thumbnail images</li>
+                            <li>Recent Profiles Module on Homepage </li>
+                            <li>Search Engine Friendly URLs</li>
+                        </ul>
+                        <h3>Registered Info</h3>
+                        <ul>
+                            <li>Membership Info</li>
+                            <li>Manage membership account info</li>
+                            <li>Order details</li>
+                        </ul>
+                        <br />
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
