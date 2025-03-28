877ed76073806218adbf801c7275fa00364a8d16
Venky <mail2venky542@gmail.com>
2025-03-28 20:33:36 +0530

    ecommerce page updated

---
+++ ecommerce_main.php
@@ -0,0 +1,84 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">E-Commerce Hosting, Customization, Installation Services</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ E-Commerce
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+                        <h2>OsCommerce:</h2>
+                        <p align="justify">OsCommerce is the most comprehensive and popular open source Shopping Cart
+                            system in existence. It has a huge list of features and is probably the most versatile
+                            shopping cart solution and offers vast range of customization choices for the on-line
+                            storefront owners depending on their need at a very affordable price</p>
+
+                        <p align="justify">
+                            osCommerce is an online shop e-commerce solution that offers a wide range of out-of-the-box
+                            features that allows online stores to be setup fairly quickly with ease, and is available
+                            for free as an Open Source based solution released under the GNU General Public License..
+                        </p>
+
+                        <p align="justify">
+                            osCommerce is a PHP and mySQL based catalog and shopping cart application that can either be
+                            directly integrated into a web site - or – a site can be built around osCommerce as a base.
+                            Its feature packed out-of-the-box installation allows store owners to maintain their online
+                            stores with minimum effort and with absolutely no costs or license fees involved.. </p>
+
+                        <h4>General Functionality</h4>
+
+                        <ul>
+                            <li>Compatible with all PHP 4 versions</li>
+                            <li>All features enabled by default for a complete out-of-the-box solution </li>
+                            <li>Completely multilingual with English, German, and Spanish provided by default</li>
+                            <li>Object oriented backend (MS3)</li>
+                        </ul>
+
+                        <h4>Setup / Installation</h4>
+                        <ul>
+                            <li>Automatic web-browser based installation and upgrade procedure</li>
+                        </ul>
+                        <h4>Shipping Functionality</h4>
+                        <ul>
+                            <li>Weight, price, and destination based shipping modules</li>
+                            <li>Real-time quotes available (UPS, USPS, FedEx, ..)</li>
+                        </ul>
+                        <br />
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
