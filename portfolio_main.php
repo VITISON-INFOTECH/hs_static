c9b249f4fdf7cf31fa00f8cdcb93ec81683deaa8
Venky <mail2venky542@gmail.com>
2025-03-28 15:58:36 +0530

    portfolio page updated

---
+++ portfolio_main.php
@@ -0,0 +1,233 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Ready Made Packs are Available</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Portfolio</div>
+                        <!-- /Page navigation-->
+                    </div>
+
+
+                </div>
+                <!-- /sub page header-->
+
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                   		<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/shreshta1.png" title="SHRESHTA INFOTECH" rel="prettyPhoto[gallery1]"><img src="images/shreshta.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http://shreshtainfotech.com/" title="">SHRESHTA INFOTECH</a></h4>
+
+							<!-- portfolio description -->
+							Launch your Company website in 24- 48 hours ! B2B trading Marketplace Script is a wonderful solution to launch your own business 
+							
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/cadpro1.png" title="CADPRO TECHNOLOGY" rel="prettyPhoto[gallery1]"><img src="images/cadpro.png" width="284" height="146" class="image portfolio" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http://www.cadpro.technology/" title="">CADPRO TECHNOLOGY</a></h4>
+
+							<!-- portfolio description -->
+							Launch your own Institute portal website in 2 days ! We have the Complete Script Ready !
+						
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+				
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/MED1.png" title="MEDFORD PHARMA" rel="prettyPhoto[gallery1]"><img src="images/med.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http://medfordpharma.com/" title="">MEDFORD PHARMA</a></h4>
+
+							<div align="justify">
+							  <!-- portfolio description -->
+							Medford Pharma has been established as a pharmaceutical company in India to offer better healthcare solutions with focus
+							
+							</div>
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/aurora1.png" title="AURORA PHARMA" rel="prettyPhoto[gallery1]"><img src="images/aurora.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http:beta.aurorapharma.in" title="">AURORA PHARMA</a></h4>
+
+							<!-- portfolio description -->
+							Aurora Pharma is a WHO GMP certified company and is manufacturing Pharmaceutical formulations since 2013
+							
+						
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/tech1.png" title="image title" rel="prettyPhoto[gallery1]"><img src="images/tech.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http://shreshtatechnologies.com/1/" title="">SHRESHTA TECHNOLOGIES</a></h4>
+
+							<!-- portfolio description -->
+							Successful project begins from its idea development and implementation planning
+							
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+				
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/saranya1.png" title="SARANYA TRAVELS" rel="prettyPhoto[gallery1]"><img src="images/saranya.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="http://saranyatravels.com/template3/index.html" title="">SARANYA TRAVELS</a></h4>
+
+							<!-- portfolio description -->
+							Founded on February 19, 2005 by SARANYA (popularly known as SARANYA ), SARANYA TOURS & TRAVELS
+							
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/FOOD1.png" title="image title" rel="prettyPhoto[gallery1]"><img src="images/FOOD.png" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="#" title="">FOOD MANAGEMENT SYSTEM</a></h4>
+
+							<!-- portfolio description -->
+							
+							Food Management system readypacks available for online food websites with userfriendly very innovative ideas
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/portfolio_big.jpg" title="image title" rel="prettyPhoto[gallery1]"><img src="images/portfolio_small.jpg" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="#" title="">HOMEOPATHY PROJECT</a></h4>
+
+							<!-- portfolio description -->
+							Homepathy projects which will be very useful to the doctors for payement modules,treatment modules and patient registration.
+							
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+				
+				<!-- box -->
+					<div class="box three">
+							<!-- portfolio image for images -->
+							<div class="imgarea portfolio">
+									<span class="magnifier"><a href="images/portfolio_big.jpg" title="image title" rel="prettyPhoto[gallery1]"><img src="images/portfolio_small.jpg" width="284" height="146" class="image portfolio" alt="" /></a></span>
+							</div>
+							<!-- / portfolio image-->
+							
+							<div class="textarea">
+							<!-- portfolio title-->
+							<h4 class="portfolio_title"><a href="#" title="">HOSTEL WEBSITES</a></h4>
+
+							<!-- portfolio description -->
+							This is our new idea which we also create hostel websites for readypacks which will be very useful to the students and  working people
+							
+							<div class="slicer"></div>
+							</div>
+					</div>
+				<!-- /box -->
+
+                   
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
