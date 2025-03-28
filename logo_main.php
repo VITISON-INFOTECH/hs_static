1acdc8e76acb9b64775e51036f2d38670799cd6a
Venky <mail2venky542@gmail.com>
2025-03-28 20:44:47 +0530

    Logo page updated

---
+++ logo_main.php
@@ -0,0 +1,233 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Logos</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Logos
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/1 (2).jpg" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">SHRESHTA INFOTECH</a></h4>
+
+                            <!-- portfolio description -->
+                            Successful project begins from its idea development and implementation planning.
+                            <a href="#" class="read_more">read more</a>
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/cadprol.png" width="284" height="146"
+                                    class="image portfolio" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">CADPRO TECHNOLOGY</a></h4>
+
+                            <!-- portfolio description -->
+                            We design your logo according to requirements and very innovative way
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/medl.png" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">MEDFORD PHARMA</a></h4>
+
+                            <!-- portfolio description -->
+                            This is pharmetical company which we design according to their requirements and in creative
+                            way
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/auroral.png" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">AURORA PHARMA</a></h4>
+
+                            <!-- portfolio description -->
+                            This is china based company designed according to their ideas and views
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/SVG.png" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">SHRESHTA TECHNOLOGIES</a></h4>
+
+                            <!-- portfolio description -->
+
+                            Shreshta Technologies this is the website we developed form scractch for academic projects
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/saranyal.png" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">SARANYA TRAVELS</a></h4>
+
+                            <!-- portfolio description -->
+                            Saranya Travels we created logo keeping it in our mind that logo should be treat like their
+                            travels name
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/3 (2).png" width="284" height="146"
+                                    class="image portfolio" alt="" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">FOOD MANAGEMENT SYSTEM</a></h4>
+
+                            <!-- portfolio description -->
+                            We Also provide readymade packs for online food system and we also do shipping
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/about.png" width="284" height="137"
+                                    class="image portfolio" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">HOMEOPATHY PROJECT</a></h4>
+
+                            <!-- portfolio description -->
+                            We also do homepath projets like modules of patient,treatment,payment module for doctors
+
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+
+                    <!-- box -->
+                    <div class="box three">
+                        <!-- portfolio image for images -->
+                        <div class="imgarea portfolio">
+                            <span class="magnifier"><img src="images/st logo.png" width="284" height="136"
+                                    class="image portfolio" /></span>
+                        </div>
+                        <!-- / portfolio image-->
+
+                        <div class="textarea">
+                            <!-- portfolio title-->
+                            <h4 class="portfolio_title"><a href="#" title="">HOSTEL WEBSITES</a></h4>
+
+                            <!-- portfolio description -->
+
+                            This is another logo for travels website and very innovative
+                            <div class="slicer"></div>
+                        </div>
+                    </div>
+                    <!-- /box -->
+                    <!--/ Page Left-->
+
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
