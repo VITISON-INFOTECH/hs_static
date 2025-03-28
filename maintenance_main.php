79f85f7653c80b93ede2481cc297a787c4474333
Venky <mail2venky542@gmail.com>
2025-03-28 20:28:51 +0530

    webmaintenance page updated

---
+++ maintenance_main.php
@@ -0,0 +1,94 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Website Maintenance</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Website Maintenance
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Website Maintenance Services in India</h2>
+                        <p align="justify">
+                            If you want your web site to work…you have to keep it maintained. As you may already know, a
+                            web site can quickly decay in performance and value. Proactively managing your site can
+                            extend its useful life, maximize its return on investment and consistently increase in
+                            marketing value for your company.Maintaining a web site is a labor intensive, complex task.
+                            To do it effectively you need a professional. We offer customized website maintenance,
+                            cheap/affordable website maintenance packages that guarantee increased performance for your
+                            site.
+                        </p>
+                        <p align="justify">In the current economic environment, companies, organizations and
+                            institutions alike are seeking ways to reduce costs, and add value to their marketing and
+                            communications strategies. Our professional Website Maintenance service deliverers just
+                            that.Once your site is online, it will inevitably need to be updated to keep your content
+                            fresh. Sometimes it's a simple change, like changing a date, or adding an event or you may
+                            want to add a new section or functionality to your site involving site-wide modification.
+                        </p>
+
+                        <h3>A web site maintenance package features the following focuses:</h3>
+                        <h4>Content</h4>
+
+                        <p align="justify">
+                            The content needs to be alive, current and accurate. With fresh information people will have
+                            a reason to return to the site time and time again. Also old articles, comments and other
+                            information should be archived. </p>
+
+                        <h4>Maintaining & managing your site:</h4>
+
+                        <p align="justify">
+                            Using newest technologies, we can monitor your site and make changes that improve your site
+                            performance for faster download and improved content spread. We insure that new innovative
+                            features are implemented from time to time. </p>
+
+                        <h4>Response and customer service
+                        </h4>
+
+                        <p align="justify">
+                            Site visitors, customers and members often need a response. They may have questions,
+                            comments, complaints and suggestions. We can handle your customer support, forum moderating
+                            and any kind of database management from here in India. </p>
+                        <h4>Maintenance Contract Plans (Annual)</h4>
+                        <ul>
+                            <li>Plan A - For websites up to 10 pages: US$6 per page.</li>
+                            <li>Plan B - For websites between 11 and 20 pages: US$ 5 per page. </li>
+                            <li>Plan C - For websites between 21 and 40 pages: US$ 4 per page.</li>
+                        </ul>
+                        <br />
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
