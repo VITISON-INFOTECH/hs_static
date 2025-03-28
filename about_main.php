1c8357b7b776ef88fd4d6b9a7a6427b39e1c3111
Venky <mail2venky542@gmail.com>
2025-03-28 15:08:43 +0530

    about main updated

--- about_main.php
+++ about_main.php
@@ -1,140 +1,162 @@
 <div id="wrapper">
     <div id="wrapper2">
 
-    <div id="wrapper_sub">
+        <div id="wrapper_sub">
 
-        <!-- header -->
-        <?php include "header-menu.html" ?>
+            <!-- header -->
+            <?php include "header-menu.html" ?>
 
-        <div class="clear"></div> 	
+            <div class="clear"></div>
 
-        <div id="wrapper2_sub">
-            <!-- sub page header-->
-            <div id="sub_page_header">
-                <div class="left">
-                    <h1 class="shadow">About Us</h1>
-                    <!-- Page navigation-->
-                    <div class="breadcrumb shadow">You are here: <a href="index.html">Home</a> \ About Us</div>
-                    <!-- /Page navigation-->
-                </div>
-
-
-            </div>
-            <!-- /sub page header-->
-
-
-            <!-- main -->
-            <div id="main">
-
-                <!-- Page Left-->
-                <div class="content">
-
-                    <h2>Hansuja Solutions</h2>
-                    <p align="justify"><a href="http://hansujasolutions.com/"
-                            title="Hansuja Solutions Software Development"><img src="images/about.png" width="220"
-                                height="137" class="alignleft" alt="Hansuja Solutions Software Development" /></a>
-                        We develop quality software and applications from 2012. Today our team delivers custom software
-                        and
-                        provides outsourced software development for start-ups and solid companies in many countries of
-                        the
-                        world. Recent years proved that Shreshta team is really stable. Our personnel turnover rate was
-                        below 10% per annum and this is quite low as compared to some other companies. </p>
-                    <p align="justify">Team’s stability is the best mark of a healthy spirit in the company and the
-                        staff’s
-                        professionalism. It points out that employees have good working conditions.Due to these facts we
-                        achieve success for each project as the chance to loose key people of the project is equal to
-                        almost
-                        zero.On the other hand out team grows every year that helps us to improve our skills by
-                        acquiring
-                        knowledge of new employees.</p>
-
-                    <h3>Reasons To Choose Hansuja Solutions</h3>
-
-                    <p align="justify">
-                        Successful project begins from its idea development and implementation planning. Before we offer
-                        a
-                        solution, Hansuja Solutions analyze the core of your project, its goals and perspectives,
-                        individual
-                        specific needs. Whatever you may need – custom software development, business management
-                        solution
-                        integration, mobile or web application development, we are here to provide you with necessary
-                        solution. Here you can get any software development life cycle services – from project idea
-                        elaboration and planning to implementation and ongoing support. </p>
-
-                    <br />
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">About Us</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.html">Home</a> \ About Us</div>
+                        <!-- /Page navigation-->
+                    </div>
 
 
                 </div>
-                <!--/ Page Left-->
-                <!-- Page Right-->
-                <div class="sidebar">
-                    <div class="sidebar_header"></div>
-                    <div class="sidebar_content">
-                        <!-- sub link-->
-                        <!--  title-->
-                        <h4 class="side_title">Our specialist services include:</h4>
-                        <ul id="sub_menu">
-                            <li><a href="http://hansujasolutions.com/solutions.html">Website Design and Development</a>
-                            </li>
-                            <li><a href="http://hansujasolutions.com/maintenance.html"> Website Maintenance</a></li>
-                            <li><a href="http://hansujasolutions.com/hosting.html">Website Hosting</a></li>
-                            <li><a href="http://hansujasolutions.com/hosting.html">Domain Name Registrations</a></li>
-                            <li><a href="http://hansujasolutions.com/logo.html">Corporate Logos</a></li>
-                            <li><a href="http://hansujasolutions.com/solutions.html">Web Marketing Consulting</a></li>
-                            <li><a href="http://hansujasolutions.com/ecommerce.html">E-Commerce Applications</a></li>
-                            <li><a href="http://hansujasolutions.com/content.html">Software Development</a></li>
-                            <li><a href="http://hansujasolutions.com/content.html">CRM/CMS/B2B</a></li>
-                            <li><a href="http://hansujasolutions.com/content.html">Wordpress</a></li>
-                            <li><a href="http://hansujasolutions.com/solutions.html">Professional Website Content
-                                    Editing</a></li>
-                            <li><a href="http://hansujasolutions.com/matimony.html">Matrimonial Portals</a></li>
-                            <li><a href="http://hansujasolutions.com/content.html">Courier/Cargo Tracking </a></li>
-                            <li><a href="http://hansujasolutions.com/online-jobs.html">Recruitment / Jobs portal
-                                    script</a>
-                            </li>
-                            <li><a href="http://hansujasolutions.com/web-training.html">Web Training</a></li>
-                            <li><a href="http://hansujasolutions.com/online-marketing.html">Online Marketing</a></li>
-                            <li><a href="http://hansujasolutions.com/bulk-sms.html">BULK SMS</a></li>
-                        </ul>
-                        <!-- /sub link-->
+                <!-- /sub page header-->
 
-                        <!-- box -->
-                        <div class="box middle">
-                            <div class="imgarea">
-                                <!-- box 1 image -->
-                                <a href="http://hansujasolutions.com/web-design.html"><img src="images/webservices.png"
-                                        width="220" height="137" class="image" alt="Web Services" /></a>
-                            </div>
-                            <div class="textarea">
-                                <!-- box title-->
-                                <h3 class="shadow"><a href="http://hansujasolutions.com/content.html">CMS / Portals</a>
-                                </h3>
-                                <div align="justify">
-                                    <!-- text-->
-                                    A content management system (CMS) supports the creation, management, distribution,
-                                    publishing, and discovery of corporate information.It covers the complete lifecycle
-                                    of
-                                    the pages on your site, from providing simple tools to create the content, through
-                                    to
-                                    publishing, and finally to archiving.
-                                    <br />
 
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /box -->
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Hansuja Solutions</h2>
+                        <p align="justify"><a href="http://hansujasolutions.com/"
+                                title="Hansuja Solutions Software Development"><img src="images/about.png" width="220"
+                                    height="137" class="alignleft" alt="Hansuja Solutions Software Development" /></a>
+                            We develop quality software and applications from 2012. Today our team delivers custom
+                            software
+                            and
+                            provides outsourced software development for start-ups and solid companies in many countries
+                            of
+                            the
+                            world. Recent years proved that Shreshta team is really stable. Our personnel turnover rate
+                            was
+                            below 10% per annum and this is quite low as compared to some other companies. </p>
+                        <p align="justify">Team’s stability is the best mark of a healthy spirit in the company and the
+                            staff’s
+                            professionalism. It points out that employees have good working conditions.Due to these
+                            facts we
+                            achieve success for each project as the chance to loose key people of the project is equal
+                            to
+                            almost
+                            zero.On the other hand out team grows every year that helps us to improve our skills by
+                            acquiring
+                            knowledge of new employees.</p>
+
+                        <h3>Reasons To Choose Hansuja Solutions</h3>
+
+                        <p align="justify">
+                            Successful project begins from its idea development and implementation planning. Before we
+                            offer
+                            a
+                            solution, Hansuja Solutions analyze the core of your project, its goals and perspectives,
+                            individual
+                            specific needs. Whatever you may need – custom software development, business management
+                            solution
+                            integration, mobile or web application development, we are here to provide you with
+                            necessary
+                            solution. Here you can get any software development life cycle services – from project idea
+                            elaboration and planning to implementation and ongoing support. </p>
+
+                        <br />
+
 
                     </div>
-                    <div class="sidebar_bottom"></div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <div class="sidebar">
+                        <div class="sidebar_header"></div>
+                        <div class="sidebar_content">
+                            <!-- sub link-->
+                            <!--  title-->
+                            <h4 class="side_title">Our specialist services include:</h4>
+                            <ul id="sub_menu">
+                                <li><a href="http://hansujasolutions.com/solutions.html">Website Design and
+                                        Development</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/maintenance.html"> Website Maintenance</a></li>
+                                <li><a href="http://hansujasolutions.com/hosting.html">Website Hosting</a></li>
+                                <li><a href="http://hansujasolutions.com/hosting.html">Domain Name Registrations</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/logo.html">Corporate Logos</a></li>
+                                <li><a href="http://hansujasolutions.com/solutions.html">Web Marketing Consulting</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/ecommerce.html">E-Commerce Applications</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/content.html">Software Development</a></li>
+                                <li><a href="http://hansujasolutions.com/content.html">CRM/CMS/B2B</a></li>
+                                <li><a href="http://hansujasolutions.com/content.html">Wordpress</a></li>
+                                <li><a href="http://hansujasolutions.com/solutions.html">Professional Website Content
+                                        Editing</a></li>
+                                <li><a href="http://hansujasolutions.com/matimony.html">Matrimonial Portals</a></li>
+                                <li><a href="http://hansujasolutions.com/content.html">Courier/Cargo Tracking </a></li>
+                                <li><a href="http://hansujasolutions.com/online-jobs.html">Recruitment / Jobs portal
+                                        script</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/web-training.html">Web Training</a></li>
+                                <li><a href="http://hansujasolutions.com/online-marketing.html">Online Marketing</a>
+                                </li>
+                                <li><a href="http://hansujasolutions.com/bulk-sms.html">BULK SMS</a></li>
+                            </ul>
+                            <!-- /sub link-->
+
+                            <!-- box -->
+                            <div class="box middle">
+                                <div class="imgarea">
+                                    <!-- box 1 image -->
+                                    <a href="http://hansujasolutions.com/web-design.html"><img
+                                            src="images/webservices.png" width="220" height="137" class="image"
+                                            alt="Web Services" /></a>
+                                </div>
+                                <div class="textarea">
+                                    <!-- box title-->
+                                    <h3 class="shadow"><a href="http://hansujasolutions.com/content.html">CMS /
+                                            Portals</a>
+                                    </h3>
+                                    <div align="justify">
+                                        <!-- text-->
+                                        A content management system (CMS) supports the creation, management,
+                                        distribution,
+                                        publishing, and discovery of corporate information.It covers the complete
+                                        lifecycle
+                                        of
+                                        the pages on your site, from providing simple tools to create the content,
+                                        through
+                                        to
+                                        publishing, and finally to archiving.
+                                        <br />
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /box -->
+
+                        </div>
+                        <div class="sidebar_bottom"></div>
+                    </div>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
                 </div>
-                <!--/  Page Right -->
 
                 <!-- footer -->
-                <?php include "footer.html" ?>
-                <!-- footer -->
-
             </div>
-
-            <!-- footer -->
         </div>
-    </div>
