a259d710417588e05382a5538bdc7f78806ea8c5
Venky <mail2venky542@gmail.com>
2025-03-28 15:29:49 +0530

    services page added

---
+++ services_main.php
@@ -0,0 +1,93 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">About Us</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.html">Home</a> \ Services</div>
+                        <!-- /Page navigation-->
+                    </div>
+
+
+                </div>
+                <!-- /sub page header-->
+
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Hansuja Solutions</h2>
+                        <p align="justify"><a href="http://hansujasolutions.com/"
+                                title="Hansuja Solutions Software Development"><img src="images/about.png" width="220"
+                                    height="137" class="alignleft" alt="Hansuja Solutions Software Development" /></a>
+                            We develop quality software and applications from 2012. Today our team delivers custom
+                            software
+                            and
+                            provides outsourced software development for start-ups and solid companies in many countries
+                            of
+                            the
+                            world. Recent years proved that Shreshta team is really stable. Our personnel turnover rate
+                            was
+                            below 10% per annum and this is quite low as compared to some other companies. </p>
+                        <p align="justify">Team’s stability is the best mark of a healthy spirit in the company and the
+                            staff’s
+                            professionalism. It points out that employees have good working conditions.Due to these
+                            facts we
+                            achieve success for each project as the chance to loose key people of the project is equal
+                            to
+                            almost
+                            zero.On the other hand out team grows every year that helps us to improve our skills by
+                            acquiring
+                            knowledge of new employees.</p>
+
+                        <h3>Reasons To Choose Hansuja Solutions</h3>
+
+                        <p align="justify">
+                            Successful project begins from its idea development and implementation planning. Before we
+                            offer
+                            a
+                            solution, Hansuja Solutions analyze the core of your project, its goals and perspectives,
+                            individual
+                            specific needs. Whatever you may need – custom software development, business management
+                            solution
+                            integration, mobile or web application development, we are here to provide you with
+                            necessary
+                            solution. Here you can get any software development life cycle services – from project idea
+                            elaboration and planning to implementation and ongoing support. </p>
+
+                        <br />
+
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
