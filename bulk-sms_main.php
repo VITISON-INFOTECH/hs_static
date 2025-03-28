33e8e2af87b6e35f39c4b54dcd0e0e641a0a796d
Venky <mail2venky542@gmail.com>
2025-03-28 21:02:03 +0530

    bulk sms page updated

---
+++ bulk-sms_main.php
@@ -0,0 +1,56 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">BULK SMS Services</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ BULK SMS Services
+
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>BULK SMS</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/bulk-sms.jpg" width="220" height="137"
+                                    class="alignleft" /></a>
+                            SMS: {Short Message Service} is a communication service standardized in the GSM mobile
+                            communication system, using standardized communications protocols allowing the interchange
+                            of short text messages between mobile telephone devices.</p>
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
