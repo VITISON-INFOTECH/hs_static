e704085204097644a43b35bebc21b569e60ac59f
Venky <mail2venky542@gmail.com>
2025-03-28 19:35:59 +0530

    solutions page updated

---
+++ solutions_main.php
@@ -0,0 +1,86 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Solutions We Offer </h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Solutions</div>
+                        <!-- /Page navigation-->
+                    </div>
+
+
+                </div>
+                <!-- /sub page header-->
+
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Website Development & Web Application Services India</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/about.png" width="220" height="137"
+                                    class="alignleft" /></a>
+                            Hansuja Solutions is a web consulting and design firm that serves fresh ideas daily. We
+                            create compelling websites that deliver the digital solutions our clients demand.
+
+                            We’re committed to service, collaboration, and communication throughout each step of the web
+                            development and design process.</p>
+                        <p align="justify">We are equipped to address all strategic and technological issues, including
+                            website development of companies with Content strategies, Corporate identity & branding,
+                            E-Commerce solutions and Multi-tiered web-based application development. Website development
+                            at also involves graphics designers who work in synergy, catering not only for the
+                            look-and-feel, but the total user experience, from the human interface to addressing user
+                            requirements and meeting web application objectives.</p>
+                        <h3>We are competent in: </h3>
+                        <ul>
+                            <li>Web Development of custom designed solutions Full-Service Web Site Development </li>
+                            <li>Web-enabled Databases development and Applications Web application development, Business
+                                to Consumer Web Sites </li>
+                            <li> E-Business and E-Commerce-enabled Web Sites development Simple "Static" Sites or
+                                Complex Interactive Sites development</li>
+                        </ul>
+                        <br />
+                        <h3>A Brief list of interactive dynamic web design modules offered by us : </h3>
+
+                        <p align="justify">Our Designers understand how important the style and functionality of your
+                            website is for the successful growth of your business online. It takes insight, experience,
+                            dedication and discipline to build successful websites.</p>
+                        <div align="justify">
+                        </div>
+                        <p align="justify">We communicate with our customers every step of the way so they get exactly
+                            what they want and more! Our web design services extend to either web hosting platform, UNIX
+                            or Windows and both MS SQL or MySQL database integration. Your Satisfaction is always
+                            Guaranteed at Hansuja Solutions</p>
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
