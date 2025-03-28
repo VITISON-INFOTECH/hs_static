6eaf82ca638257fdc43a622f921f5a25d98351fd
Venky <mail2venky542@gmail.com>
2025-03-28 20:58:50 +0530

    online marketing pages are updated

---
+++ online-marketing_main.php
@@ -0,0 +1,129 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Online Marketing Services</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Online Marketing
+                            Services
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+
+                        <h2>Search Engine Optimization (SEO)</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/search-engine-optimization.png" width="220"
+                                    height="137" class="alignleft" /></a>
+                            SEO is required to optimize your website and to showcase your products, services and
+                            solutions on the front page of Google and other leading search engines.</p>
+                        <p align="justify">SEO is a marketing discipline focused on growing visibility in organic
+                            (non-paid) search engine results. SEO encompasses both the technical and creative elements
+                            required to improve rankings, drive traffic, and increase awareness in search engines.
+                            <br>
+                        </p>
+                        <p align="justify">Benefits <br />
+
+                        <ul>
+                            <li>Increase Traffic and Visitors to your Website. </li>
+                            <li>Better Search Engines Visibility. </li>
+                            <li>Improve Business Productivity. </li>
+                            <li>Takes your Business to the Next Level. </li>
+                            <li>Get Known Globally and Locally. </li>
+                        </ul>
+                        </p>
+
+                        <p align="justify">Measuring ROI <br />
+
+                        <ul>
+                            <li>Traffic is the amount of visitors or hits your website gets</li>
+                            <li>Keyword ranking metrics </li>
+                            <li>Page Ranking </li>
+                            <li>Back links </li>
+                            <li>Conversions are website visitors who turn into customers </li>
+                        </ul>
+                        </p>
+                        <h2>Social Media Optimization (SMO)</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/Social-Media-Optimization-(SMO).jpg" width="220"
+                                    height="137" class="alignleft" /></a>
+                            Social media optimization (SMO) is the use of a number of outlets and communities to
+                            generate publicity to increase the awareness of a product, service brand or event. Types of
+                            social media involved include RSS feeds, social news and bookmarking sites, as well as
+                            social networking sites, such as Facebook, Twitter, video sharing websites and blogging
+                            sites. SMO is similar to search engine optimization, in that the goal is to generate web
+                            traffic and to a site and increase awareness for a website.</p>
+                        <p align="justify">The goal of SMO is to strategically create interesting online content,
+                            ranging from well-written text to eye-catching digital photos or video clips that encourages
+                            and entices people to engage with a website and then share this content, via its weblink,
+                            with their social media contacts and friends.
+                        </p>
+
+
+                        <h2>Social Media Marketing(SMM)</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/Social-Media-Marketing-(SMM).jpg" width="220"
+                                    height="137" class="alignleft" /></a>
+                            Social media marketing (SMM) is a form of Internet marketing that utilizes social networking
+                            websites as a marketing tool. The goal of SMM is to produce content that users will share
+                            with their social network to help a company increase brand exposure and broaden customer
+                            reach.</p>
+                        <p align="justify"> SMM became more common with the increased popularity of websites such as
+                            Twitter, Facebook, Blogging, LinkedIn, and YouTube etc…
+
+                            Facebook
+                            Twitter
+                            LinkedIn
+                            Google+
+                            Pinterest
+                            YouTube
+                            Blogging etc</p>
+
+                        <h2>Pay-Per-Click (PPC)</h2>
+                        <p align="justify"><a rel="prettyPhoto[gallery1]" title="Image Title"
+                                href="images/box3.png"><img src="images/pay_per_click.jpg" width="220" height="137"
+                                    class="alignleft" /></a>
+                            Pay-per-click is commonly associated with first-tier search engines (such as Google AdWords
+                            and Microsoft Bing Ads). With search engines, advertisers typically bid on keyword phrases
+                            relevant to their target market. In contrast, content sites commonly charge a fixed price
+                            per click rather than use a bidding system. </p>
+                        <p align="justify"> PPC “display” advertisements, also known as “banner” ads, are shown on web
+                            sites with related content that have agreed to show ads and are typically not pay-per-click
+                            advertising. Social networks such as Facebook and Twitter have also adopted pay-per-click as
+                            one of their advertising models.</p>
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
