9a63ee83bf4514b7ae711b8ca71afd616b25dc80
Venky <mail2venky542@gmail.com>
2025-03-28 19:47:47 +0530

    contactus header footer page are updated

---
+++ contactus.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Contact us | Hansuja Solutions</title>
+
+<meta name="Description" content="Hansuja Solutions Hyderabad team delivers custom software and provides outsourced software development for start-ups and solid companies in many countries of the world."/> 
+
+<meta name="Keywords" content="Website Design and Development, Website Maintenance, Website Hosting, Domain Name Registrations, Corporate Logos, Web Marketing Consulting, E-Commerce Applications, Software Development, CRM/CMS/B2B, Wordpress"> 
+
+<meta name="distribution" content="GLOBAL" /> 
+
+<meta name="robots" content="index, follow" /> 
+
+<meta name="classification" content="Website Design and Development, Website Maintenance, Website Hosting, Domain Name Registrations, Corporate Logos, Web Marketing Consulting, E-Commerce Applications, Software Development, CRM/CMS/B2B, Wordpress" /> 
+
+<meta name="abstract" content="Website Design and Development, Website Maintenance, Website Hosting, Domain Name Registrations, Corporate Logos, Web Marketing Consulting, E-Commerce Applications, Software Development, CRM/CMS/B2B, Wordpress"> 
+
+<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/style_cf.css" />
+<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css" media="screen" />
+<link rel="shortcut icon" href="images/logo1.png">
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.1.1.js"></script>
+<script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+<script type="text/javascript" src="js/jquery.validate.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+<!--[if IE 6]>
+<script type='text/javascript' src='js/dd_belated_png.js'></script>
+<script>DD_belatedPNG.fix('.png');</script>
+<![endif]-->
+<!-- /* Social Icons */ -->
+
+<style type="text/css">
+		
+#social_side_links {
+  position: fixed;
+  top: 200px;
+  right: 0;
+  padding: 0;
+  list-style: none;
+  z-index: 99;
+}
+
+#social_side_links li a {display: block;}
+
+#social_side_links li a img {
+	display: block;
+	max-width:40px;
+  padding: 10px;
+  -webkit-transition:  background .2s ease-in-out;
+  -moz-transition:  background .2s ease-in-out;
+  -o-transition:  background .2s ease-in-out;
+  transition:  background .2s ease-in-out;
+}
+
+
+
+/* Demo Text */
+.demo-text {
+	font-family: helvetica, sans-serif;
+  font-weight: 100;
+  font-size: 30px;
+  line-height: 43px;
+  color: #444;
+  padding: 10px;
+}
+	.demo-text--special {
+		background-color:#A79C8E;
+     color: #fff;
+     padding: 30px;
+	}
+	
+	.demo-text--small {
+		font-size: 23px;
+     line-height: 37px;
+	}
+
+h1.demo-text {
+  margin: 40px 0 0 0;
+  line-height: 0;
+  font-size: 50px;
+}
+
+a.demo-text {
+	text-decoration:none;
+  font-size: 20px;
+}
+
+img{padding: 0px;}  
+
+	</style>
+<!-- /* Social Icons */ -->
+</head>
+<body>
+    <!-- parallax background -->
+    <div id="parallax">
+        <div id="background1"></div>
+        <div id="background2"></div>
+    </div>
+    <!-- parallax background -->
+
+    <!-- main  -->
+    <?php include "contactus_main.php" ?>
+    <!-- main  -->
+
+    <!-- Social Icons -->
+    <?php include "social-icons.html" ?>
+    <!-- Social Icons -->
+
+</body>
+</html>
---
+++ contactus_main.php
@@ -0,0 +1,110 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+            <div class="clear"></div>
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Contact Us </h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Contact Us</div>
+                        <!-- /Page navigation-->
+                    </div>
+
+                </div>
+                <!-- /sub page header-->
+
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+                        <!-- contact form -->
+                        <div id="contact_form">
+                            <form name="form" id="form" method="post" action="send.php">
+                                <ul>
+                                    <li><label for="name">Your name: (*)</label><input id="name" type="text"
+                                            name="name" /><span id="errorname" style="color: red;"></span></li>
+                                    <li><label for="email">Your Email: (*)</label><input id="email" type="text"
+                                            name="email" /><span id="erroremail" style="color: red;"></span></li>
+                                    <li><label for="phone">Phone Number: (*) </label><input id="phone" type="text"
+                                            name="phone" /> <span id="errorphone" style="color: red;"></span> </li>
+                                    <li><label for="message">Your message: (*)</label><textarea id="message"
+                                            name="message" rows="8" cols="40"></textarea><span id="errormessage"
+                                            style="color: red;"></span></li>
+                                    <li><input type="submit" class="button" value="Send" onclick="return validate()" />
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                        <!-- /contact form -->
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <!-- Page Right-->
+                    <div class="sidebar">
+                        <div class="sidebar_header"></div>
+                        <div class="sidebar_content">
+
+
+                            <!-- box -->
+                            <h4 class="side_title">Contact Us</h4>
+                            <div class="box middle">
+                                <div class="textarea">
+                                    <!-- sub link -->
+                                    <ul>
+                                        <li><b>Address:
+                                                <pre><b>2-9-180/11 , New Bharath Nagar<br/> Near KKR Function Hall Lane, <br/>Uppal , Hyderabad , Telangana,<br/> 500039 India</b></pre>
+                                        </li>
+                                        <li><b>Contact No : </b> +91 7842478495 <br /></li>
+                                        <li><b>E-Mail: </b> info@hansujasolutions.com</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- /box -->
+
+                            <!-- box -->
+                            <h4 class="side_title">Google Map</h4>
+                            <div class="box middle">
+                                <div class="textarea">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15228.488722325936!2d78.5661667!3d17.4059232!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x54857b9e243aa44a!2sHansuja+Solutions!5e0!3m2!1sen!2sin!4v1502003625440"
+                                        width="300" height="250" frameborder="0" style="border:0"
+                                        allowfullscreen></iframe>
+
+
+
+
+                                </div>
+                            </div>
+                            <!-- /box -->
+
+
+
+                        </div>
+                        <div class="sidebar_bottom"></div>
+                    </div>
+                    <!--/  Page Right -->
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
