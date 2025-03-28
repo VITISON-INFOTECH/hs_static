c876cec7c72bae0f558eda122d4825401736501a
Venky <mail2venky542@gmail.com>
2025-03-28 20:39:38 +0530

    content page updated

---
+++ content_main.php
@@ -0,0 +1,75 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">A Simple CMS</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ CMS
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+                <!-- main -->
+                <div id="main">
+                    <!-- Page Left-->
+                    <div class="content">
+                        <h2>A Simple Content Management Solution</h2>
+                        <p align="justify">Content management systems (CMS) store and manage an organization's
+                            electronic document and Web content so that even the non-technical authors and employee of
+                            the company can reuse the information across different applications which is otherwise done
+                            by technical programmers. The web published content can also be distributed to customers and
+                            business partners outside the organization.
+                        </p>
+                        <p align="justify">
+                            Content management system enables you to establish a consistent look and feel throughout
+                            your site, but gives your non-technical content authors the power to publish and update
+                            their own content using simple, but powerful, browser-based tools. Some of the CMS systems
+                            integrate with content delivery applications to deliver the content via a web site. </p>
+
+                        <p align="justify">
+                            With numerous OPEN SOURCE Content Management Tools available on the web, none of them was
+                            SIMPLE. Designers4Web always emphazises on Simplicity. We felt the need of a Content
+                            Management System (CMS) thats is pretty straight forward </p>
+
+                        <h4>Features</h4>
+
+                        <ul>
+                            <li>Single Page Template </li>
+                            <li>Title, Keywords for SEO </li>
+                            <li>A Simple Admin Panel</li>
+                            <li>Add / Edit Pages </li>
+                        </ul>
+
+                        <br />
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
