7c05471cc12cb8e6737a9774549e0dc82ce2b00b
Venky <mail2venky542@gmail.com>
2025-03-28 21:28:33 +0530

    seo page updated

---
+++ online-jobs.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Online Jobs |Software Development | Testing | Services | Hyderabad</title>
+
+<meta name="Description" content="Hansuja Solutions is one of The Best Software Development & Software Testing Services in Hyderabad. We offer Bulk SMS,CRM,CMS,Courier,Cargo Tracking in Hyderabad"/> 
+
+<meta name="Keywords" content="software development services hyderabad, software testing services hyderabad"> 
+
+<meta name="distribution" content="GLOBAL" /> 
+
+<meta name="robots" content="index, follow" /> 
+
+<meta name="classification" content="software development services hyderabad, software testing services hyderabad" /> 
+
+<meta name="abstract" content="software development services hyderabad, software testing services hyderabad"> 
+
+<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/style_cf.css" />
+<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css" media="screen" />
+<link rel="shortcut icon" href="images/logo1.png">
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.1.1.js"></script>
+<script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+<script type="text/javascript" src="js/jquery.validate.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+<!--[if IE 6]>
+<script type='text/javascript' src='js/dd_belated_png.js'></script>
+<script>DD_belatedPNG.fix('.png');</script>
+<![endif]-->
+<!-- /* Social Icons */ -->
+
+<style type="text/css">
+		
+#social_side_links {
+  position: fixed;
+  top: 200px;
+  right: 0;
+  padding: 0;
+  list-style: none;
+  z-index: 99;
+}
+
+#social_side_links li a {display: block;}
+
+#social_side_links li a img {
+	display: block;
+	max-width:40px;
+  padding: 10px;
+  -webkit-transition:  background .2s ease-in-out;
+  -moz-transition:  background .2s ease-in-out;
+  -o-transition:  background .2s ease-in-out;
+  transition:  background .2s ease-in-out;
+}
+
+
+
+/* Demo Text */
+.demo-text {
+	font-family: helvetica, sans-serif;
+  font-weight: 100;
+  font-size: 30px;
+  line-height: 43px;
+  color: #444;
+  padding: 10px;
+}
+	.demo-text--special {
+		background-color:#A79C8E;
+     color: #fff;
+     padding: 30px;
+	}
+	
+	.demo-text--small {
+		font-size: 23px;
+     line-height: 37px;
+	}
+
+h1.demo-text {
+  margin: 40px 0 0 0;
+  line-height: 0;
+  font-size: 50px;
+}
+
+a.demo-text {
+	text-decoration:none;
+  font-size: 20px;
+}
+
+img{padding: 0px;}  
+
+	</style>
+<!-- /* Social Icons */ -->
+</head>
+
+<body>
+    <!-- parallax background -->
+    <div id="parallax">
+        <div id="background1"></div>
+        <div id="background2"></div>
+    </div>
+    <!-- parallax background -->
+
+    <!-- main  -->
+    <?php include "online-jobs_main.php" ?>
+    <!-- main  -->
+
+    <!-- Social Icons -->
+    <?php include "social-icons.html" ?>
+    <!-- Social Icons -->
+
+</body>
+</html>
---
+++ online-jobs_main.php
@@ -0,0 +1,81 @@
+<div id="wrapper">
+    <div id="wrapper2">
+
+        <div id="wrapper_sub">
+
+            <!-- header -->
+            <?php include "header-menu.html" ?>
+
+            <div class="clear"></div>
+
+            <div id="wrapper2_sub">
+                <!-- sub page header-->
+                <div id="sub_page_header">
+                    <div class="left">
+                        <h1 class="shadow">Online Jobs Portal / Recruitment Website Script in PHP and MYSQL</h1>
+                        <!-- Page navigation-->
+                        <div class="breadcrumb shadow">You are here: <a href="index.php">Home</a> \ Online Jobs
+                        </div>
+                        <!-- /Page navigation-->
+                    </div>
+                </div>
+                <!-- /sub page header-->
+
+                <!-- main -->
+                <div id="main">
+
+                    <!-- Page Left-->
+                    <div class="content">
+                        <h2>
+                            Online Jobs/Resume/ Recruitment Management System </h2>
+                        <p align="justify">This is the best solution if you are looking to run an online jobs/resume
+                            website. We have integrated many features which you can use and take full advantage of the
+                            online jobs portal.</p>
+                        <p align="justify">
+                            This is a very powerful online Online Jobs/Resume management package system that we've
+                            developed exclusively and user friendly. User will be able to search, update, add/remove,
+                            and edit their resumes/profiles from database. In addition, user can also add/delete/change
+                            descriptions, upload images/photos.
+
+                        </p>
+
+                        <h4>Job Seeker’s Area - Employer’s Area - Admin Area</h4>
+
+                        <ul>
+                            <li>Home Page Design</li>
+                            <li>Static Pages </li>
+                            <li>Registration – Log In</li>
+                            <li>Edit Resume – Email Resume – Print Resume</li>
+                        </ul>
+                        <h4>Admin Area</h4>
+                        <ul>
+                            <li>Inactive Applicants – Convert to Active</li>
+                            <li>Inactive Employers – Convert Free to Paid</li>
+                            <li>Post Featured Jobs</li>
+                            <li>Paid Employers Report</li>
+                            <li>Renew Paid Users</li>
+                            <li>User address Look Up, Employers Address Look Up</li>
+                        </ul>
+
+                        <br />
+
+                    </div>
+                    <!--/ Page Left-->
+                    <!-- Page Right-->
+                    <?php include "page-right.html" ?>
+                    <!--/  Page Right -->
+                    <div class="clear"></div>
+                    <div class="cont4">
+
+                        <h4 align="center" style="color:#E82F00; padding-top:10px; "></h4>
+
+                    </div>
+                    <!-- footer -->
+                    <?php include "footer.html" ?>
+                    <!-- footer -->
+
+                </div>
+
+                <!-- footer -->
+            </div>
+        </div>
